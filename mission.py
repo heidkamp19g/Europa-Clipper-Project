1fea70597cb3ca0aabf2e18b85a32786684fd78b
vllin <39091846+vllin@users.noreply.github.com>
2018-05-18 14:43:29 -0400

    Update mission.py
    http://www.astronomynotes.com/starprop/totalvel.gif

--- mission.py
+++ mission.py
@@ -18,7 +18,7 @@ solar_system_ephemeris.set("jpl")
 ## Initial data
 # Links and sources: https://github.com/poliastro/poliastro/wiki/EuroPython:-Per-Python-ad-Astra
 date_launch = Time("2011-08-05 16:25", scale='utc')
-C_3 = 31.1 * u.km**2 / u.s**2
+C_3 = 31.1 * u.km**2 / u.s**2 #specific orbital energy when orbiting Earth's surface (en.wikipedia.org/wiki/Specific_Orbital_Energy)
 date_flyby = Time("2013-10-09 19:21", scale='utc')
 date_arrival = Time("2016-07-05 03:18", scale='utc')
 
@@ -34,36 +34,37 @@ ss_efly = Orbit.from_body_ephem(Earth, date_flyby)
 r_efly, v_efly = ss_efly.rv()
 
 # Assume that the insertion velocity is tangential to that of the Earth
-dv = C_3**.5 * v_e0 / norm(v_e0)
-man = Maneuver.impulse(dv)
+dv = C_3**.5 * v_e0 / norm(v_e0) #project sqrt(C_3) onto unit vector tangential to Earth for change in velocity
+man = Maneuver.impulse(dv) #set Poliastro "maneuver" to the change in velocity dv
 
 # Inner Cruise 1
-ic1 = ss_e0.apply_maneuver(man)
+ic1 = ss_e0.apply_maneuver(man) #set inner orbit to leave initial state in Earth's orbit with defined "maneuver" at each step
 ic1.rv()
 
-ic1.period.to(u.year)
+ic1.period.to(u.year) #convert period of inner cruise 1 orbit in years
 
 op = OrbitPlotter()
 
-op.plot(ss_e0)
-op.plot(ic1)
+op.plot(ss_e0) #plot Earth's orbit
+op.plot(ic1) #plot inner cruise 1 orbit
 
 # We propagate until the aphelion
-ss_aph = ic1.propagate(ic1.period / 2)
-ss_aph.epoch
+ss_aph = ic1.propagate(ic1.period / 2) #aphelion: the point in the orbit at which it is furthest from the sun = ic1.period/2
+ss_aph.epoch #return time of aphelion
 
 # Let's compute the Lambert solution to do the flyby of the Earth
-time_of_flight = date_flyby - ss_aph.epoch
+# Lambert's problem is concerned with the determination of an orbit from two position vectors and the time of flight
+time_of_flight = date_flyby - ss_aph.epoch # time of aphelion to flyby of Earth
 time_of_flight
 
-(v_aph, v_fly), = izzo.lambert(Sun.k, ss_aph.r, ss_efly.r, time_of_flight)
+# Determine the orbit from gravitational constant of sun, position of aphelion, position of flyby, and time of flight
+(v_aph, v_fly), = izzo.lambert(Sun.k, ss_aph.r, ss_efly.r, time_of_flight) 
 
-# Check the delta-V
-norm(v_aph - ss_aph.v)  # Too high!
+# Check the difference between initial velocity necessary for orbit and velocity at aphelion
+norm(v_aph - ss_aph.v)  # Difference is too great...intermediate orbit defined below
 
-
-ss_aph_post = Orbit.from_vectors(Sun, ss_aph.r, v_aph, epoch=ss_aph.epoch)
-ss_junofly = Orbit.from_vectors(Sun, r_efly, v_fly, epoch=date_flyby)
+ss_aph_post = Orbit.from_vectors(Sun, ss_aph.r, v_aph, epoch = ss_aph.epoch)
+ss_junofly = Orbit.from_vectors(Sun, r_efly, v_fly, epoch = date_flyby)
 
 op = OrbitPlotter()
 
